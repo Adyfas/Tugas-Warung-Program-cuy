a18615bd507c8a438a816212409785c039d5be27
Adyfas <adymycode@gmail.com>
2024-03-21 12:35:37 +0700

    Create tugas.py

---
+++ Bab2/tugas.py
@@ -0,0 +1,13 @@
+def bikinbintang(h):
+        #buatkan program python untuk membuat output setengah segitiga contoh:
+#*
+#**
+#***
+#****
+#*****
+
+#jika kamu lihat di funsi bikinbintang itu kan ada parameter ya ? nah itu gunanya untuk menentukan seberapa panjangnya segitiga yang akan di buat begitu
+
+if __name__=="__main__":
+        h = 10
+        bikinbintang()
